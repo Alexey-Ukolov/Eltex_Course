7f18e7eee689daf64c941a7ed601b4fa962ce24d
Alexey-Ukolov <ua5054931@gmail.com>
2026-04-28 17:53:58 +0700

    lesson complete

---
+++ scripts/blog.js
@@ -0,0 +1,60 @@
+const createBtn = document.getElementById("createBtn");
+const statsBtn = document.getElementById("statsBtn");
+const form = document.getElementById("createForm");
+const cancelBtn = document.getElementById("cancelBtn");
+const addPostBtn = document.getElementById("addPostBtn");
+
+const dialog = document.getElementById("statsDialog");
+const closeDialog = document.getElementById("closeDialog");
+const postCountEl = document.getElementById("postCount");
+
+const container = document.querySelector(".article-container");
+
+let postCount = document.querySelectorAll(".blog-article").length;
+
+createBtn.addEventListener("click", () => {
+    form.classList.add("show");
+});
+
+cancelBtn.addEventListener("click", () => {
+    form.classList.remove("show");
+});
+
+addPostBtn.addEventListener("click", () => {
+    const template = `
+        <article class="blog-article">
+            <div class="blog-article-img">
+                <img src="./assets/images/placeholder.png" alt="mock">
+            </div>
+            <div class="blog-article-text">
+                <h4 class="blog-article-title">Новая статья</h4>
+                <p class="blog-article-description">Тестовый текст статьи...</p>
+                <time>${new Date().toLocaleDateString()}</time>
+            </div>
+        </article>
+    `;
+
+    container.insertAdjacentHTML("beforeend", template);
+
+    postCount++;
+});
+
+statsBtn.addEventListener("click", () => {
+    postCountEl.textContent = postCount;
+    dialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    dialog.close();
+});
+
+dialog.addEventListener("click", (e) => {
+    const rect = dialog.getBoundingClientRect();
+    const isOutside =
+        e.clientX < rect.left ||
+        e.clientX > rect.right ||
+        e.clientY < rect.top ||
+        e.clientY > rect.bottom;
+
+    if (isOutside) dialog.close();
+});
